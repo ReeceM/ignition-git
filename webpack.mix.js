33df5c3596f78d54dd2d8612763e36b51d5bd575
ReeceM <reecestevenmay@gmail.com>
2019-09-01 19:11:38 +0200

    packages and styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,25 @@
 let mix = require('laravel-mix')
+const tailwindcss = require('tailwindcss');
 
-mix.js('resources/js/tab.js', 'dist/js');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project 
+  content: [
+    './resources/**/*.html',
+    './resources/**/*.vue',
+    './resources/**/*.js',
+    './resources/**/*.php'
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
+mix.js('resources/js/tab.js', 'dist/js')
+    .options({
+        processCssUrls: false,
+        postCss: [
+        tailwindcss('./tailwind.config.js'),
+        ...process.env.NODE_ENV === 'production' ? [purgecss] : []
+        ],
+    });
